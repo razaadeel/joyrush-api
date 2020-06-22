281857a2eda5813dadda88bded4daa35731bd092
Max <Max@bizzwc.local>
2020-06-22 21:35:02 +0500

    config changes

--- config/db.js
+++ config/db.js
@@ -5,10 +5,9 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://admin:admin@mern-n1n1y.mongodb.net/test?retryWrites=true&w=majority", { useUnifiedTopology: true,useNewUrlParser: true, useCreateIndex:true });
+        await mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
         console.log('Database Connected...');
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err.message);
         //process exit with failure
         process.exit(1);
